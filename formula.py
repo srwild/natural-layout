6104ed27d793109668f0e0103512857718076c61
S.R. Wild <git@srwild.com>
2019-10-26 12:32:12 -0400

    add page dimension functions

---
+++ formula.py
@@ -0,0 +1,24 @@
+signWidthInches = 11
+signHeightInches = 5.75
+
+
+def roundConvertToPoints(dimension):
+
+  # Round inches to nearest half inch
+  if dimension % 1 >= .5:
+    dimension = int(dimension) + 1
+  else:
+    dimension = int(dimension)
+
+  # Covnvert to points
+  dimension = dimension * 72
+
+  return dimension
+
+
+horizontalAxis = roundConvertToPoints(signWidthInches)
+verticalAxis = roundConvertToPoints(signHeightInches)
+
+
+
+size(horizontalAxis, verticalAxis)
