407d615d4fb81ff778d86a3b1fa49ba8f1b4006e
S.R. Wild <git@srwild.com>
2019-10-26 13:38:31 -0400

    add optical centerer

--- formula.py
+++ formula.py
@@ -32,5 +32,26 @@ A = findShortestAxis() * .15
 B = verticalAxis * .15
 
 
+opticalCenter = horizontalAxis / 2, verticalAxis * .54
+
+def drawopticalCenter():
+    circleSize = 10
+    circleSizeHalf = circleSize / 2
+
+    with savedState():
+        fill(1)
+        cmykStroke(1, 0, 0, 0)
+        strokeWidth(1)
+        translate(opticalCenter[0] - circleSize / 2, opticalCenter[1] - circleSize / 2)
+        oval(0, 0, circleSize, circleSize)
+        line((-circleSizeHalf, circleSizeHalf), (circleSize + circleSizeHalf, circleSizeHalf))
+        line((circleSizeHalf, circleSize + circleSizeHalf), (circleSizeHalf, -circleSizeHalf))
+
+
+
 
 size(horizontalAxis, verticalAxis)
+
+drawopticalCenter()
+
+# saveImage("sheet.pdf")
